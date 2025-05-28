63b56d8d7446306038028c104d5986d9b3f62465
Jakob Kielland <jakob-ek@hotmail.com>
2025-05-28 18:30:46 +0200

    make page

---
+++ src/components/wizard.tsx
@@ -0,0 +1,125 @@
+import { useRef, useState } from "react";
+
+function padInteger(n: number) {
+  return n.toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+}
+
+const Wizard = () => {
+  const [peopleInMeeting, setPeopleInMeeting] = useState(10);
+  const [duration, setDuration] = useState(0);
+  const [liveTimerActive, setLiveTimerActive] = useState(false);
+  const [hourlyCost, setHourlyCost] = useState(100);
+  const intervalRef = useRef<NodeJS.Timeout>(undefined);
+
+  return (
+    <>
+      <div>
+        <span className="mr-2">Number of people in meeting:</span>
+        <input
+          type="number"
+          className="border-b-4 border-peachy-dark w-16 text-center"
+          value={peopleInMeeting > 0 ? peopleInMeeting : ""}
+          onChange={(e) => {
+            const value = e.target.value;
+            if (!isNaN(+value)) {
+              setPeopleInMeeting(+value);
+            }
+          }}
+        />
+      </div>
+
+      {peopleInMeeting > 0 && !liveTimerActive ? (
+        <>
+          <div>
+            <span className="mr-2">Duration of meeting:</span>
+            <span>
+              <input
+                type="number"
+                className="border-b-4 border-peachy-dark w-16 mr-2 text-center"
+                value={duration > 0 ? Math.floor(duration / 60) : ""}
+                onChange={(e) => {
+                  const value = e.target.value;
+                  if (!isNaN(+value)) {
+                    setDuration(+value * 60);
+                  }
+                }}
+              />
+              <span>mins</span>
+            </span>
+          </div>
+        </>
+      ) : null}
+
+      {peopleInMeeting > 0 && liveTimerActive ? (
+        <div>
+          <div>Time elapsed:</div>
+          <div>
+            {padInteger(Math.floor(duration / 3600))}:
+            {padInteger(Math.floor(duration / 60) % 60)}:
+            {padInteger(duration % 60)}
+          </div>
+        </div>
+      ) : null}
+
+      {(peopleInMeeting > 0 && duration > 0) || liveTimerActive ? (
+        <div>
+          <div className="text-8xl">
+            ${Math.round((peopleInMeeting * duration * hourlyCost) / 60 / 60)}
+          </div>
+        </div>
+      ) : null}
+
+      <div className="grow" />
+
+      {peopleInMeeting > 0 && !liveTimerActive ? (
+        <div className="text-2xl mx-auto">
+          <span className="mr-4">Or</span>
+          <button
+            className="bg-peachy-surface hover:bg-peachy-surface-hover active:bg-peachy-surface-active rounded-xl w-fit max-w-72 px-8 py-4"
+            onClick={() => {
+              setLiveTimerActive(true);
+              const intervalID = setInterval(() => {
+                setDuration((oldDuration) => oldDuration + 1);
+              }, 1000);
+              intervalRef.current = intervalID;
+            }}
+          >
+            start a live counter
+          </button>
+        </div>
+      ) : null}
+
+      {liveTimerActive ? (
+        <button
+          className="mx-auto text-2xl bg-peachy-surface hover:bg-peachy-surface-hover active:bg-peachy-surface-active rounded-xl w-fit max-w-72 px-8 py-4"
+          onClick={() => {
+            setLiveTimerActive(false);
+            clearInterval(intervalRef.current);
+          }}
+        >
+          Stop live counter
+        </button>
+      ) : null}
+
+      <div className="text-lg font-normal italic mx-auto">
+        <span>Assumes an hourly labor cost of $</span>
+        <input
+          type="number"
+          className="border-b-1 border-peachy-dark w-10 mr-1 text-center"
+          value={hourlyCost > 0 ? hourlyCost : ""}
+          onChange={(e) => {
+            const value = e.target.value;
+            if (!isNaN(+value)) {
+              setHourlyCost(+value);
+            }
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Wizard;
